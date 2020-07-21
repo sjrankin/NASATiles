86582c87f5a37b34e46b0f66c5b8f06c2ece4781
Stuart Rankin <sjrankin@gmail.com>
2020-07-21 16:46:34 +0900

    Initial Commit

---
+++ NASATiles/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NASATiles
+//
+//  Created by Stuart Rankin on 7/21/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
