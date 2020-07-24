32cc0ba6420006ea7393c5d9ad8372ba709c1dbf
Stuart Rankin <sjrankin@gmail.com>
2020-07-24 21:08:23 +0900

    Create ImageMergeParameters.swift
    Parameters for merging images.

---
+++ NASATiles/ImageMergeParameters.swift
@@ -0,0 +1,16 @@
+//
+//  ImageMergeParameters.swift
+//  NASATiles
+//
+//  Created by Stuart Rankin on 7/24/20.
+//
+
+import Foundation
+import simd
+
+struct ImageMergeParameters
+{
+    let ImageCount: simd_uint1
+    let XOffset: simd_uint1
+    let YOffset: simd_uint1
+}
