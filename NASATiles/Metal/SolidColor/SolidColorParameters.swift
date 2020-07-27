a48ef09670afc0e566404f8f557d331d72a9d777
Stuart Rankin <sjrankin@gmail.com>
2020-07-27 21:19:05 +0900

    Create SolidColorParameters.swift
    Parameters for creating solid color images.

---
+++ NASATiles/Metal/SolidColor/SolidColorParameters.swift
@@ -0,0 +1,15 @@
+//
+//  SolidColorParameters.swift
+//  NASATiles
+//
+//  Created by Stuart Rankin on 7/25/20.
+//
+
+import Foundation
+import AppKit
+import simd
+
+struct SolidColorParameters
+{
+    let Fill: simd_float4
+}
